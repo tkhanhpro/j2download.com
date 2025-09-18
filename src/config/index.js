823b84e28c8049231ab5b1e19d7c34e04a8e3365
tkhanhpro <tkhanh.dev@gmail.com>
2025-09-18 18:19:30 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ export default {
     BASE_URL: process.env.NODE_ENV === 'production' ? 'https://tk-down.vercel.app' : 'http://localhost:4002',
     API_URL: process.env.NODE_ENV === 'production' ? 'https://api.tk-down.vercel.app/v1/' : 'http://localhost:4002/v1/',
     API_URL_DOWNLOAD: process.env.NODE_ENV === 'production' ? 'https://api.tk-down.vercel.app/download/?url=' : 'http://localhost:4002/download/?url=',
-    SOCKET_ENDPOINT: process.env.NODE_ENV === 'production' ? 'https://socket.tk-down.vercel.app/' : 'https://socket.j2download.com/',
+    SOCKET_ENDPOINT: process.env.NODE_ENV === 'production' ? 'https://socket.tk-down.vercel.app/' : 'http://socket.localhost:4002/',
 }
