21464e1cdb735e7b711378af4749340a1e37c71f
tkhanhpro <tkhanh.dev@gmail.com>
2025-09-18 17:48:30 +0700

    Update AutoBot.vue

--- src/components/Container/AutoBot.vue
+++ src/components/Container/AutoBot.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h3 class="title f16">{{ $t("container.auto_bot") }}</h3>
-    <p>Telegram Bot: <a href="https://j2c.cc/telegram_bot" ><font color="#0000FF">J2 Bot ✅</font></a></p>
-    <p>Facebook Bot: <a href="https://j2c.cc/fb_bot" ><font color="red">Bot ꕤ</font></a></p>
+    <p>Telegram ChatBot : <a href="https://j2c.cc/telegram_bot" ><font color="#0000FF">J2 Bot ✅</font></a></p>
+    <p>Facebook ChatBot: <a href="https://j2c.cc/fb_bot" ><font color="red">Bot ꕤ</font></a></p>
   </div>
 </template>
 
