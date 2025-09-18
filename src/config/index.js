f0d07e26156085d2e7ae02998cca2aaeab54b05e
tkhanhpro <tkhanh.dev@gmail.com>
2025-09-18 17:42:15 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,7 @@
 export default {
     J2DOWN_SECRET: import.meta.env.VITE_J2DOWN_SECRET,
-
-    BASE_URL: process.env.NODE_ENV === 'production' ? 'https://j2download.com/' : 'http://localhost:4002',
-    API_URL: process.env.NODE_ENV === 'production' ? 'https://api.j2download.com/v1/' : 'http://localhost:4002/v1/',
-    API_URL_DOWNLOAD: process.env.NODE_ENV === 'production' ? 'https://api.j2download.com/download/?url=' : 'http://localhost:4002/download/?url=',
-    SOCKET_ENDPOINT: process.env.NODE_ENV === 'production' ? 'https://socket.j2download.com/' : 'https://socket.j2download.com/',
+    BASE_URL: process.env.NODE_ENV === 'production' ? 'https://tk-down.vercel.app/' : 'http://localhost:4002',
+    API_URL: process.env.NODE_ENV === 'production' ? 'https://api.tk-down.vercel.app/v1/' : 'http://localhost:4002/v1/',
+    API_URL_DOWNLOAD: process.env.NODE_ENV === 'production' ? 'https://api.tk-down.vercel.app/download/?url=' : 'http://localhost:4002/download/?url=',
+    SOCKET_ENDPOINT: process.env.NODE_ENV === 'production' ? 'https://socket.tk-down.vercel.app/' : 'https://socket.j2download.com/',
 }
